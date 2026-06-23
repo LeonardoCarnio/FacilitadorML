a3501f1810377c39fa72a79a25fde77a78b6448e
LeonardoCarnio <132392266+LeonardoCarnio@users.noreply.github.com>
2026-06-23 14:19:25 -0300

    Create ml_refresh.py

---
+++ api/ml_refresh.py
@@ -0,0 +1,16 @@
+import json
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps({
+                "error": "refresh_token ainda não implementado"
+            }).encode()
+        )
