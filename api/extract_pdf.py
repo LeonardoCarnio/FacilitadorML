495ace5b7f635b9aaf846bc208963af6326d1f02
LeonardoCarnio <132392266+LeonardoCarnio@users.noreply.github.com>
2026-06-23 12:37:09 -0300

    Update extract_pdf.py

--- api/extract_pdf.py
+++ api/extract_pdf.py
@@ -1,1 +1,17 @@
+from http.server import BaseHTTPRequestHandler
+import json
 
+class handler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps({
+                "success": True,
+                "message": "API funcionando"
+            }).encode()
+        )
