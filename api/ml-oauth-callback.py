02050dd4985639da12a0d85edafc5573806116f0
LeonardoCarnio <132392266+LeonardoCarnio@users.noreply.github.com>
2026-06-23 11:54:21 -0300

    Update ml-oauth-callback.py

--- api/ml-oauth-callback.py
+++ api/ml-oauth-callback.py
@@ -1,5 +1,15 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "OAuth callback funcionando"
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps({
+                "status": "ok",
+                "message": "OAuth callback funcionando"
+            }).encode()
+        )
