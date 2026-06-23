c1cfd0e410fe67f6cc1a72fa0a7ab752dc08652a
LeonardoCarnio <132392266+LeonardoCarnio@users.noreply.github.com>
2026-06-23 13:11:14 -0300

    Update ml_search.py

--- api/ml_search.py
+++ api/ml_search.py
@@ -1,1 +1,24 @@
+import json
+import requests
+from http.server import BaseHTTPRequestHandler
 
+class handler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+
+        content_length = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(content_length)
+
+        data = json.loads(body)
+
+        query = data.get("query", "")
+
+        url = f"https://api.mercadolibre.com/sites/MLB/search?q={query}"
+
+        response = requests.get(url)
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(response.text.encode())
